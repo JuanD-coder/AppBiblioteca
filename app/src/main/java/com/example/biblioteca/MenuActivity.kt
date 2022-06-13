1d638b1d9a1d418635acdf6c01b69005e4414588
JuanDavid2923 <gutirrezvidalj@gmail.com>
2022-06-13 16:29:25 -0500

    Actualizacion del proyecto

--- app/src/main/java/com/example/biblioteca/MenuActivity.kt
+++ app/src/main/java/com/example/biblioteca/MenuActivity.kt
@@ -1,11 +1,13 @@
 package com.example.biblioteca
 
 import android.content.Context
+import android.content.DialogInterface
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import androidx.appcompat.app.AlertDialog
 import com.example.biblioteca.data.RegistrarUser
 import com.example.biblioteca.databinding.ActivityMenuBinding
 import com.example.biblioteca.perfil.Pefil
@@ -20,12 +22,21 @@ class MenuActivity : AppCompatActivity() {
         title = ""
 
         vinculo.user.setOnClickListener{
-            startActivity(Intent(this,RegistrarUser::class.java))
+            showDelate("Antes de registar un usuario debe de registar un tipo de Usuario","Aceptar")
+            startActivity(Intent(this, RegistrarUser::class.java))
         }
+
 
         vinculo.tipo.setOnClickListener{
             startActivity(Intent(this,tipoActivity::class.java))
         }
+    }
+    private fun showDelate(titulo: String, mensaje: String){
+        val alert = android.app.AlertDialog.Builder(this@MenuActivity)
+        alert.setTitle(titulo)
+        alert.setMessage(mensaje)
+        alert.setCancelable(true)
+        alert.show()
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
